9666789329a33b3963b827f946e4522fb755f4c7
JosueEspinoS <99145912+JosueEspinoS@users.noreply.github.com>
2022-09-26 11:06:31 -0500

    Create main.vb

---
+++ VB .NET POO/main.vb
@@ -0,0 +1,8 @@
+Module VBModule 'este programa de VB se llama VBModule.
+ 
+    Sub Main()'define una función principal que se ejecuta primero.
+        Console.WriteLine("Hello World! desde VB")'imprime el resultado
+        Console.ReadLine()'se asegura de ver el resultado.
+    End Sub'termina la funcion principal Main()
+  
+End Module' fin del programa VB'
